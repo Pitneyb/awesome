b1f56fff7c200612c7852983b99a6e4cfa85c619
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2021-02-25 10:08:57 +0700

    Changed default Terminal, and added some startup programs

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -3,14 +3,16 @@ local filesystem = require('gears.filesystem')
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'lxterminal',
+    terminal = 'konsole',
     browser = 'firefox'
   },
   
    -- List of apps to start once on start-up
   run_on_start_up = {
-    '/usr/bin/lxpolkit &', -- credential manager
-    '/usr/bin/picom &'	--
-    'firefox &'
+    '/usr/bin/lxpolkit', 	 -- credential manager
+    '/usr/bin/picom -b --config ~/picom.conf',	 --
+    'firefox',			 -- Default Browser
+    'thunderbird',		 -- Email program
+    'konsole'			 -- Default Terminal
   }
 }
