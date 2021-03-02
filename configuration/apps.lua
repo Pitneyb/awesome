0fec39f3d41e58a350f73d79c970f78680c3c8fe
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2021-03-02 11:19:08 +0700

    Added some programs to auto-start

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -9,10 +9,13 @@ return {
   
    -- List of apps to start once on start-up
   run_on_start_up = {
-    '/usr/bin/lxpolkit', 	 -- credential manager
-    '/usr/bin/picom -b --config ~/picom.conf',	 --
+    'lxpolkit', 	 -- credential manager
+    'picom -b --config ~/picom.conf',	 --
     'firefox',			 -- Default Browser
     'thunderbird',		 -- Email program
-    'konsole'			 -- Default Terminal
+    'konsole',			 -- Default Terminal
+    'clementine --log-level 0',	 -- run Music player with reduced logging
+    'volumeicon',		 -- volume control in sys-tray
+    'nm-applet'			 -- Network sys-tray
   }
 }
